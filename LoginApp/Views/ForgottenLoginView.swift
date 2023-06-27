576993a40601dbef129c12af9e59b673c500e18a
Susanna <susanna1235@gmail.com>
2023-06-28 00:11:01 +0300

    прописала логику для экрана который выходит если ты забыл пароль, это было увлекательно ахахха

--- LoginApp/Views/ForgottenLoginView.swift
+++ LoginApp/Views/ForgottenLoginView.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ForgottenLoginView: UIViewController {
+    //MARK: кнопочки
+    @IBOutlet var helpButton: UIButton!
+    @IBOutlet var resetPassword: UIButton!
+    //MARK: текст
     
-
+    @IBOutlet var adviceText: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
- 
+        
+        adviceText.isHidden = true
+        
     }
-
+    
+    var tapCount = 0
+    
+    
+    //MARK: логика для кнопки подсказки
+    @IBAction func helpButtonAction(_ sender: Any) {
+        adviceText.isHidden = false
+        tapCount += 1
+        switch tapCount % 4 {
+        case 0:
+            adviceText.text = "Девичья фамилия матери"
+        case 1:
+            adviceText.text = "Имя друга детства"
+        case 2:
+            adviceText.text = "Прозвище в школе"
+        case 3:
+            adviceText.text = "Видимо все таки нужно сбросить пароль..."
+        default:
+            break
+        }
+    }
 }
+
